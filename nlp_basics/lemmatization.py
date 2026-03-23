aabf8607d1d91ec142056b3df9622606c2e66420
anmolgupta2704 <2k23.cscys2312044@gmail.com>
2026-03-23 11:20:14 +0530

    updated

--- nlp_basics/lemmatization.py
+++ nlp_basics/lemmatization.py
@@ -1,5 +1,7 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import SnowballStemmer
 
 nltk.download('wordnet')
 
@@ -10,3 +12,23 @@ words = ["running", "better", "cars"]
 lemmatized = [lemmatizer.lemmatize(word) for word in words]
 
 print(lemmatized)
+tt = TweetTokenizer()
+ss = SnowballStemmer("english")
+
+text = "loving nlp !!! #ai"
+tokens = tt.tokenize(text)
+
+print(tokens)
+print([ss.stem(t) for t in tokens])
+
+
+text = "I love running"
+tokens = tt.tokenize(text)
+
+print(tokens)
+print([ss.stem(t) for t in tokens])
+text = "My studies are going on"
+tokens = tt.tokenize(text)
+
+print(tokens)
+print([ss.stem(t) for t in tokens])
