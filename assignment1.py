be2e0dfe86e67b5257bc2c452d0fe2905d4f968a
anmolgupta2704 <2k23.cscys2312044@gmail.com>
2026-03-23 11:17:59 +0530

    updated

---
+++ assignment1.py
@@ -0,0 +1,40 @@
+import nltk
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+feedback = [
+    'the faculty was very supportive and the lectures were really helpful',
+    'i am not satisfied with the lab sessions',
+    'the courser content is good',
+    'lan were not working properly'
+]
+
+ss = SnowballStemmer("english")
+
+tokens_list = []
+
+# Tokenization
+for sentence in feedback:
+    tokens = word_tokenize(sentence)
+    tokens_list.append(tokens)
+
+print("Tokens:", tokens_list)
+
+# Stopword removal
+stop_word = set(stopwords.words('english'))
+filtered_tokens = []
+
+for tokens in tokens_list:
+    filtered = [word for word in tokens if word.lower() not in stop_word]
+    filtered_tokens.append(filtered)
+
+print("After Stopword Removal:", filtered_tokens)
+
+stemmed_tokens = []
+for tokens in filtered_tokens:
+    stemmed = [ss.stem(word) for word in tokens]
+    stemmed_tokens.append(stemmed)
+
+print("After Stemming:", stemmed_tokens)
