35ff5c335eefdef603b4b0f6faad433fc3cd3ab2
anmolgupta2704 <2k23.cscys2312044@gmail.com>
2026-04-10 16:43:56 +0530

    update

--- ANN.py
+++ ANN.py
@@ -68,15 +68,23 @@ df = pd.DataFrame(data)
 # X = df.drop('label', axis=1)
 # y = df['label']
 # # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 # print("Training Features:\n", X_train)
 # print("\nTraining Labels:\n", y_train)
 # print("\nTesting Features:\n", X_test)
 # print("\nTesting Labels:\n", y_test)
 
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-sc=StandardScaler()
-from sklearn.compose import ColumnTransformer
-x_train=sc.fit_transform(x_train)
-x_test=sc.transform(x_test)
+# from sklearn.preprocessing import StandardScaler, OneHotEncoder
+# sc=StandardScaler()
+# from sklearn.compose import ColumnTransformer
+# x_train=sc.fit_transform(x_train)
+# x_test=sc.transform(x_test)
+import tensorflow as tf
+tf.random.set_seed(42)
+ann=tf.keras.models.Sequential()
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
